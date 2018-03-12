1b83c60feb46c1828823e75ff1e9940f7e8f1908
eric <eric@mail.com>
2018-03-12 23:28:27 +0700

    all backend stuff done ish

--- lavelle/app/Models/Transaction.php
+++ lavelle/app/Models/Transaction.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
   protected $table="transactions";
-  protected $fillable=["category_id","customer_id","item_id","qty","total_price"];
+  protected $fillable=["customer_id","item_id","qty","total"];
   protected $guarded=[];
+
+  //declare relations to perhaps get item name and price
+  public function itemsrc(){
+    return $this->belongsTo("App\Models\Item","item_id");
+  }
+  public function customersrc(){
+    return $this->belongsTo("App\User","customer_id");
+  }
 }
--- lavelle/database/migrations/2014_10_12_000000_create_users_table.php
+++ lavelle/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,9 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('admin')->nullable();
+            $table->string('phone_num');
+            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
